be4083bc2f44e65142b328533f2d8ab05ca50998
arcturus611 <swati.6.itaws@gmail.com>
2014-10-25 01:29:10 -0700

    remove second string (easy)

---
+++ c16e_remove_second_string.c
@@ -0,0 +1,30 @@
+	//r/dailyprogrammer challenge 16 easy
+	//remove string characters
+	//gcc -g -std=c99 -Wall c16e_remove_second_string.c -o c16e_remove_second_string
+	//To run: ./c16e_remove_second_string String1 String2
+	
+	#include<stdio.h>
+	#include<string.h>
+	
+	void remove_second_string(char* s1, char* s2){
+		char c;
+		int l1 = strlen(s1);
+		
+		for (int i = 0; i<l1; i++){
+			c = s1[i];
+			if(strchr(s2, c)==0) printf("%c", c);
+		}
+		
+		printf("\n");
+		
+		return;
+	}
+	
+	int main(int argc, char* argv[]){
+		char* str1 = argv[1]; //note you can't initialize as char str1[] = argv[1]
+		char* str2 = argv[2]; 
+	
+		remove_second_string(str1, str2);
+		
+		return 0;
+	}
