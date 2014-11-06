ec77b442954297413b52ae357d0908aa1fa9fa65
arcturus611 <swati.6.itaws@gmail.com>
2014-11-05 23:28:59 -0800

    remove vowels (easy)

---
+++ c149e_disemvoweler.c
@@ -0,0 +1,41 @@
+	//r/dailyprogrammer challenge 149 (easy)
+	//disemvoweler
+	
+	#include<stdio.h>
+	#include<ctype.h>
+	#include<stdlib.h>
+	#include<string.h>
+	
+	#define MAX_LEN 100
+	
+	void disemvowel(char* line){
+		char* p = line;
+		line[strchr(line, '\n') - line] = '\0';
+		char* cons_line = malloc(MAX_LEN*sizeof(char)); char* c = cons_line;
+		char* vowels_line = malloc(MAX_LEN*sizeof(char)); char* v = vowels_line;
+		
+		while(*p){
+			if(!isspace(*p)){
+				if((*p)!='a' && (*p)!='e' && (*p)!='i' && (*p)!='o' && (*p)!='u')
+					*c++ = *p;			
+				else
+					*v++ = *p;
+			}
+			p++;
+		}
+		*c = '\0';
+		*v = '\0';
+		
+		printf("The disemboweled string is %s, and the vowels are %s\n", cons_line, vowels_line);
+		
+		return;
+	}
+	
+	int main(int argc, char* argv[]){
+		char* line = malloc(MAX_LEN*sizeof(char));
+		fgets(line, MAX_LEN, stdin);
+		
+		disemvowel(line);
+		
+		return 0;
+	}
