da553e4d64d35e0b14fe92ef116c21cf512f9068
arcturus611 <swati.6.itaws@gmail.com>
2014-11-22 00:14:13 -0800

    complete hangman

--- c189e_hangman.c
+++ c189e_hangman.c
@@ -11,6 +11,28 @@
     "street", "mountain", "space", "yellow", "python", "computer", "print", "string", 
     "xylophone", "scrabble", "lumberjack", "doppelganger", "conscientious", "predetermined"};
     
+    	char* hangman[8] = {"-----------", "     |     ", "     o     ", "     |     ", "    -|     ", "    -|-    ", "    -|     ", "    -|-    "};
+    	
+    	void draw_hangman(int n){
+    		printf("%s\n%s\n", hangman[0], hangman[1]);
+    		
+    		switch (n){
+    			case 1: printf("%s\n", hangman[2]);
+    				break;
+    			case 2: printf("%s\n%s\n", hangman[2], hangman[3]);
+    				break;
+    			case 3: printf("%s\n%s\n", hangman[2], hangman[4]);
+    				break;
+    			case 4: printf("%s\n%s\n", hangman[2], hangman[5]);
+    				break;
+    			case 5: printf("%s\n%s\n%s\n", hangman[2], hangman[5], hangman[6]);
+    				break;
+    			case 6: printf("%s\n%s\n%s\n", hangman[2], hangman[5], hangman[7]);
+    				break;
+    		}
+    		return;
+    	}
+    
     	void play_hangman(char* str){
     		int num_tries = 6; //head, torso, 2 hands, 2 legs
     		int num_guessed_correct = 0;
@@ -43,9 +65,7 @@
 	    			}else
 	    				break;
     			}
-    			if(!curr_guess){
-    				num_tries--;
-    			}
+    			
     			for(int i = 0; i<l; i++){
     				if(mark_guesses[i]){
     					printf("%c ", str[i]);
@@ -53,11 +73,16 @@
     				else printf("_ ");
     			}
     			printf("\n");
+    			if(!curr_guess){
+    			    	num_tries--;
+    				draw_hangman(6 - num_tries);
+			}
     		}
     			
     		if(num_guessed_correct<l){
     			printf("Sorry, next time! Word was: %s\n", str);
     		}
+    		
     		else printf("You won!\n");
     	}
     
