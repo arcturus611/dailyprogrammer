c3703ac79dbe6dba89494e2f903d1bb055cfc99c
arcturus611 <swati.6.itaws@gmail.com>
2015-01-21 23:23:50 -0800

    quick sort

--- quickSort.c
+++ quickSort.c
@@ -1,4 +1,8 @@
-
+#include <stdio.h>
+#include <stdlib.h> // for srand, rand
+#include <time.h>
+//#include "quickSort.h"
+#define LENGTH 10 
 
 void swap(int* x, int idx1, int idx2){
 	int temp = x[idx1]; 
@@ -7,7 +11,7 @@ void swap(int* x, int idx1, int idx2){
 }
 
 int partition(int* a, int end1, int end2){
-	int pivot = (end1+end2)/2; 
+	int pivot = end2; //(end1+end2)/2; 
 	int i;
 	int newPivot = end1;  // this is what we need to calculate and return
 
@@ -26,17 +30,52 @@ int partition(int* a, int end1, int end2){
 }
 
 
-void quicksort(int* a, int end1, int end2){
+void quicksort(int* a, int end1, int end2, int array_size){
 	if (end2-end1<=0){
 		return;
 	}
 
 	int newPivot; 
 	newPivot = partition(a, end1, end2);
-	
-	quicksort(a, end1, newPivot-1); 
 
-	quicksort(a, newPivot+1, end2); 
+	for(int i = 0; i<array_size; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+	
+	quicksort(a, end1, newPivot-1, array_size); 
+
+	quicksort(a, newPivot+1, end2, array_size); 
 	
 	return; 
+}
+
+int main(int argc, char** argv){
+
+/*
+	int x[LENGTH]; 
+	int i;
+	srand(time(NULL)); 
+
+//	printf(" \n The given array is :\n "); 
+	for(i =0; i< LENGTH; i++){
+		x[i] = rand()%50 + 1; 
+		printf("%d ", x[i]); 
+	}
+	printf("\n"); */
+	
+	int _ar_size;
+	scanf("%d", &_ar_size);
+	int x[_ar_size], _ar_i;
+		
+	for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) { 
+		scanf("%d", &x[_ar_i]); 
+	}	
+
+	quicksort(x, 0, _ar_size-1, _ar_size);
+
+/*	printf(" \n The sorted array is : "); 
+	for(i =0; i< LENGTH; i++){
+		printf("%d ", x[i]); 
+	}
+	printf("\n");*/ 
 }
