2458516df6f422021d7c13c7fd804a6f8048a112
arcturus611 <swati.6.itaws@gmail.com>
2015-01-24 00:06:09 -0800

    base negative generalized

--- c198i_basenegative.c
+++ c198i_basenegative.c
@@ -4,70 +4,70 @@
 	
 	#include<stdio.h>
 	#include<stdlib.h>
+	#include<string.h>
 	#include<math.h>
+	#define MAX_LEN 100
 	
-	int convert_to_decimal(int n, int r){
-		int x, a;
-		double idx;
-		a = 0, idx = 0;
-		
-		while(n){
-			x = n%10;
-			a+= x*pow(r, idx);
-			idx++;
-			n/=10;
+	/*The idea is: when a number is in base b, it can be written as a polynomial in powers of b. To change it to base -b, this polynomial needs to be rewritten in powers of -b. */
+	int convert_to_opp_polarity_base(int n, int b){
+		int start_sub, CARRY_VAL;
+		int nega_base = 0; 
+		if(b<0){
+			nega_base = 1;
+			b = -b;
+			char* str = (char *)malloc(MAX_LEN*sizeof(char));
+			sprintf(str, "%d", n);
+			if(strlen(str)%2) start_sub = 1;
+			else start_sub = 0; 
+			//odd length string means negative number going from neg to pos base
+			CARRY_VAL = -1;
+		}else{
+			if(n<0){
+				n = -n; 
+				start_sub = 0;
+			}else{
+				start_sub = 1;
+			}
+			CARRY_VAL = 1;
 		}
 		
-		return a;
-	}
-	
-	int convert_from_decimal(int n, int b){
-		int x, a; 
-		double idx; 
-		a = 0, idx = 0;
-		
-		while(n){
-			x = n%b;
-			a+= x*pow(10, idx); 
-			n/=b;
-			idx++;
-		}
-		
-		return a;
-	}
-	
-	int convert_to_neg(int n, int b){
 		int output = 0;
 		int x, y; 
 		int place_value = 0; 
 		int carry = 0;
 		
-		if(n<0){
-			n = -n; //the sign will screw things up in the algorithm; the programmer knows it's negative;
-		}
-		else{
-			output = n%10; //don't do the subtraction on the zeroth position digit. 
+		if(start_sub){ //zeroth position digit to be ignored for subtraction
+			output = n%10; 
 			n/=10; 	
 			place_value++;		
 		}	
 		
-		while(n | carry){			
+		while(n | abs(carry)){			
 			x = n%10; 
 			x+=carry; 
 			
-			if(x==0) y = 0; // in case of zero, no subtraction.
-			else y = (b-x);
+			if(x==0){
+				y = 0; // in case of zero, no subtraction.
+				carry = 0;
+			}else{
+				y = (b-x);
+				carry = CARRY_VAL;
+			}
 
 			output+= y*pow(10, place_value);
 			place_value++;
 			n/=10;
 			
+			//next digit
 			x = n%10; 
-			if(y) x++; //because you are adding from previous subtraction
+			x+=carry; //because you are add/sub-ing from previous subtraction
 			
 			if(x==b){
 				y = 0; 
-				carry = 1;
+				carry = CARRY_VAL;
+			}else if (x==-1){
+				y = b-1; 
+				carry = 1; 
 			}else{ 
 				y = x;
 				carry = 0;
@@ -78,19 +78,15 @@
 			n/=10;
 		}
 		
-		return output;
+		return ((!start_sub) & nega_base) ? (-1)*output : output;
 	}
 	
 	int main(int argc, char* argv[]){
 		int r, n; 
 		r = atoi(argv[1]); 
 		n = atoi(argv[2]);
-	
-		if(r<0){	
-			int a = convert_to_decimal(n, r);
-			printf("From base %d to base %d, we get %d to %d\n", r, -r, n, convert_from_decimal(a, -r));	
-		}
-		else 
-			printf("From base %d to base %d, we get %d to %d\n", r, -r, n, convert_to_neg(n, r));	
+			
+		printf("Converting %d from base %d to %d gives %d\n", n, r, -r, convert_to_opp_polarity_base(n, r));
 		return 0;
 	}
+	
