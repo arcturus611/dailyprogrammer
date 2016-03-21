9f8db2e64cd11a6231e51d89342aa802f4d5b704
arcturus611 <swati.6.itaws@gmail.com>
2016-03-21 14:49:50 -0700

    ip address euc distance

---
+++ c259e_clarence_the_slow_typist/c259e_slow_typist.c
@@ -0,0 +1,8 @@
+	/* Your task is to write a program that calculates the distance Clarence must move his finger to type in arbitrary IP addresses. */
+	
+	int main (int argc, char* argv[]){
+			
+		
+			
+		return 0;
+	}
