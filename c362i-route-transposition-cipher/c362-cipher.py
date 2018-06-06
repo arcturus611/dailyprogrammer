8db22d3fcdc4d486a177d819101ef2661114aae0
arcturus611 <swati.6.itaws@gmail.com>
2018-06-06 11:28:07 -0700

    list comprehension

--- c362i-route-transposition-cipher/c362-cipher.py
+++ c362i-route-transposition-cipher/c362-cipher.py
@@ -10,22 +10,17 @@ import numpy as np
 
 #%% 
 def create_matrix(input_str, r, c):
-    input_list_str = list(input_str.upper())
+    input_upper_chars = [char.upper() for char in input_str if char.isalpha()]
     input_matrix = np.ndarray((r, c))
     for i in np.arange(r): 
         for j in np.arange(c):
             input_matrix[i][j] = ord('X')
-    k = 0
-    for i in np.arange(r): 
-        for j in np.arange(c):
-            if (k < len(input_list_str)):
-                while (input_list_str[k].isalpha()==0):
-                    k = k+1
-                    if (k == len(input_list_str)):
-                        return input_matrix
-                input_matrix[i][j] = ord(input_list_str[k].upper())
-                k = k+1          
-            
+    
+    for k in np.arange(len(input_upper_chars)):
+        i = np.floor_divide(k, c)
+        j = np.remainder(k, c)
+        input_matrix[i][j] = ord(input_upper_chars[k])
+           
     return input_matrix
 
 #%% test created matrix
@@ -54,9 +49,10 @@ def alphabetize(arr):
     return mycode
 #%% 
 if __name__ == '__main__':
-    r = 3
-    c = 9
-    created_matrix = create_matrix("We are discovered. Flee at once!!", r, c)
+    r = 12
+    c = 4
+    input_str = "For lunch let's have peanut-butter and bologna sandwiches"
+    created_matrix = create_matrix(input_str, r, c)
     encoded_numbers = spiralizer(created_matrix, r, c)
     final_code = alphabetize(encoded_numbers)
-    print(final_code)
+    print("The encoded string for \"{}\" is {}".format(input_str, final_code))
